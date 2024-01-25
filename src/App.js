1d2f65c7b06cb40878d1a85414bd048add7696c8
JAY VARDHAN VASHISHTHA <jayvashishtha.agra@gmail.com>
2024-01-25 10:37:32 +0530

    Succesfully Digital_Clock created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import DigitalClock from "./Time.jsx";
+function App() {
+  return (
+   <>
+   <DigitalClock>
+
+   </DigitalClock>
+   <DigitalClock>
+
+   </DigitalClock>
+   <DigitalClock>
+
+   </DigitalClock>
+   </>
+  );
+}
+
+export default App;
